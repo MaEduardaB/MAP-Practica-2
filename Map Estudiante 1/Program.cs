510c5c7fc33cfe5b53953330c25e4aa6c43c2d7c
Maria Eduarda Beckers <mbeckers@ucm.es>
2024-03-02 19:37:56 +0100

    Cambios de Estudiante B

--- Map Estudiante 1/Program.cs
+++ Map Estudiante 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Map_Estudiante_1 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Pa!");
         }
     }
 }
